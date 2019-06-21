f80064c03807ed15c1b1fb8ba897436a2168e4a7
naufal mukhbit <nmukhbit@gmail.com>
2019-06-21 10:45:11 +0700

    Initialization

---
+++ nlu_model.py
@@ -0,0 +1,11 @@
+from rasa_nlu.training_data import load_data
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+from rasa_nlu.model import Metadata, Interpreter
+
+training_data = load_data("./data/data.md")
+
+trainer = Trainer(config.load("config_embedding.json"))
+trainer.train(training_data)
+model_directory = trainer.persist("./models/nlu/", fixed_model_name="farm_nlu")
+interpreter = Interpreter.load(model_directory)
