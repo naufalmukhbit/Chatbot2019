c402d97aacb17ef9c78d12dc2e6a7cd5faeaff05
Bintang Peryoga <bintangperyoga.bp@gmail.com>
2019-07-08 00:06:18 +0700

    xxx

--- action.py
+++ action.py
@@ -9,13 +9,18 @@ class ActionTanam(Action):
         return 'action_tanam'
 
     def run(self, dispatcher, tracker, domain):
+        plant = tracker.get_slot('plant')
 
-        response = """ tanam tinggal tanam, goblog!""".format()
+        response = """ tanam {} tinggal tanam, goblog!""".format(plant)
         dispatcher.utter_message(response)
         return [SlotSet('plant', plant)]
 
-
+    # Step-by-step running program
+    # 1. pip install rasa_nlu[tensorflow]
+    # 2. pip install rasa_core
+    # 3. run nlu_model
+    # 4. run train_rasacore
     # terminal code syntax
     # python -m rasa_core_sdk.endpoint --actions action    <- this is bot's brain
-    # python -m rasa_core.run -d models/dialogue -u models/nlu/default/weather_nlu --port 5002 --endpoints endpoints.yml --credentials credentials.yml
+    # python -m rasa_core.run -d models/dialogue -u models/nlu/default/farm_nlu --port 5002 --endpoints endpoints.yml --credentials credentials.yml
     # python -m rasa_core.train interactive -o models/dialogue -d weather_domain.yml -s data/stories.md --nlu models/nlu/default/weather_nlu --endpoints endpoints.yml
